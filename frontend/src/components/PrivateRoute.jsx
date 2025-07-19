70c8d0043dc19fbccd55ad1ff074f7e579c786b7
AdityaNPatil <adityapatil2708@gmail.com>
2025-07-19 18:13:30 +0530

    Private Route created

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,11 +1,21 @@
-/*All protected Routes are nested insided PrivateRoute*/
+import { useSelector } from "react-redux"
+import { Navigate, Outlet, useLocation } from "react-router-dom"
 
 function PrivateRoute() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const token = useSelector(state => state.auth.token)  // auth is the name of the reducer in store.js
+  const location = useLocation()                        // get current route location useful for redirecting back after login.
+  return token ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />
 }
 
-export default PrivateRoute
+export default PrivateRoute;
+
+/*All protected Routes are nested insided PrivateRoute*/
+/*
+BRIEF EXPLANATION -> 
+
+const location = useLocation(); gets the current route location using React Router, useful for redirecting back after login.
+
+state={{ from: location }} => passes the current location to the login route so the user can be redirected back after logging in.
+
+replace => ensures the navigation history does not retain the protected page, preventing access via the browser back button.
+*/

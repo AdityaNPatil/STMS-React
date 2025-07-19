7f4b2c48412fcfe758ccbab2637056796125315e
AdityaNPatil <adityapatil2708@gmail.com>
2025-07-19 17:30:44 +0530

    preconfigure axios

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -5,6 +5,7 @@ const axiosInstance = axios.create({
     baseURL: "https://demoBackendApi/api",
 })
 
+// allows to modify request before its sent 
 axiosInstance.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token")
@@ -15,3 +16,11 @@ axiosInstance.interceptors.request.use(
 )
 
 export default axiosInstance
+
+/*
+Uses a base URL (https://demoBackendApi/api) -- for all get,put,post,delete requests made through this axiosInstance
+
+Automatically adds a Bearer token from localStorage to all outgoing requests (if token exists)
+
+Makes token-based authenticated HTTP requests easy and consistent throughout application
+*/
